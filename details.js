292e11f76d977d585eb9910ec78b045f59670c0a
indian786786 <75921809+indian786786@users.noreply.github.com>
2026-03-05 22:42:52 +0530

    Create details.js

---
+++ details.js
@@ -0,0 +1,11 @@
+// Login Details Storage
+// This file stores all login information
+// Created once, updated as people login
+
+var details = [
+    // Details will be added here as people login
+    // Format: { username: "...", password: "...", timestamp: "...", date: "...", time: "..." }
+];
+
+// Access details in browser console with: details
+
